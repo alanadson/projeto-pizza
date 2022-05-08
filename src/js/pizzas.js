87dbfd54e413e5d115928248e8de1d7162edcecf
Alan Adson <aalanadson@gmail.com>
2022-05-08 02:54:04 -0300

    changed description

--- src/js/pizzas.js
+++ src/js/pizzas.js
@@ -4,7 +4,7 @@ let pizzaJson = [{
         img: 'images/pizza.png',
         price: 20.19,
         sizes: ['100g', '530g', '860g'],
-        description: 'Descrição da Pizza'
+        description: 'Descrição Pizza'
     },
     {
         id: 2,
@@ -12,7 +12,7 @@ let pizzaJson = [{
         img: 'images/pizza2.png',
         price: 18.00,
         sizes: ['320g', '530g', '860g'],
-        description: 'Descrição da Pizza'
+        description: 'Descrição Pizza'
     },
     {
         id: 3,
@@ -20,7 +20,7 @@ let pizzaJson = [{
         img: 'images/pizza3.png',
         price: 17.45,
         sizes: ['320g', '530g', '860g'],
-        description: 'Descrição da Pizza'
+        description: 'Descrição Pizza'
     },
     {
         id: 4,
@@ -28,7 +28,7 @@ let pizzaJson = [{
         img: 'images/pizza4.png',
         price: 19.77,
         sizes: ['320g', '530g', '860g'],
-        description: 'Descrição da Pizza'
+        description: 'Descrição Pizza'
     },
     {
         id: 5,
@@ -36,7 +36,7 @@ let pizzaJson = [{
         img: 'images/pizza5.png',
         price: 21.43,
         sizes: ['320g', '530g', '860g'],
-        description: 'Descrição da Pizza'
+        description: 'Descrição Pizza'
     },
     {
         id: 6,
@@ -44,7 +44,7 @@ let pizzaJson = [{
         img: 'images/pizza6.png',
         price: 18.55,
         sizes: ['320g', '530g', '860g'],
-        description: 'Descrição da Pizza'
+        description: 'Descrição Pizza'
     },
     {
         id: 7,
@@ -52,6 +52,6 @@ let pizzaJson = [{
         img: 'images/pizza7.png',
         price: 22.36,
         sizes: ['320g', '530g', '860g'],
-        description: 'Descrição da Pizza'
+        description: 'Descrição Pizza'
     }
 ];
